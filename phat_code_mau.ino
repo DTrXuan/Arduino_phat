74625f243b6d1ede30fa6f4ca365d99c370b75da
dox2hc <xuan.doantruong@vn.bosch.com>
2022-03-13 13:51:45 +0700

    upload git hub

--- phat_code_mau.ino
+++ phat_code_mau.ino
@@ -3,8 +3,14 @@
 #define bangtai_o   5
 #define bom_o       6
 
-// giả sử khi có chai, cảm biến trả về true. không có chai trả về false.
-bool cambien_st =false; // khai báo biến lưu trạng thái cảm biến
+#define KHONG_DOI    0
+#define XUNG_LEN     1
+#define XUNG_XUONG   2
+
+// khai báo biến lưu trạng thái cảm biến
+int cambien_st   = KHONG_DOI;
+bool cambien_cur  = false;
+bool cambien_last = false;
 
 void setup() {
 
@@ -26,27 +32,38 @@ void setup() {
   pinMode(bom_o,OUTPUT);  // Cấu hình chân 5 output cấp xung điều khiển driver động cơ bơm
   analogWrite(bangtai_o,0);  // Xuất xung pwm với duty = (0)*0.255%. Duty càng lớn tốc độ càng nhanh. range: 0 -> 255
 
-  cambien_st = (bool)digitalRead(cambien_i); // Đọc trạng thái cảm biến
+  cambien_cur = (bool)digitalRead(cambien_i); // Đọc trạng thái cảm biến
+  cambien_last = (bool)digitalRead(cambien_i);
 }
 
 void loop() {
   
+  cambien_cur = (bool)digitalRead(cambien_i);
 
-  // phát hiện xung cạnh lên của cảm biến. tức là đang false -> true 
-  if (cambien_st == false && (bool)digitalRead(cambien_i) == true)
+  /* Đọc và xử lý tín hiệu cảm biến */
+  if (cambien_cur == false & cambien_last == true)
   {
-    /* code */
+      cambien_st = XUNG_LEN;
+      cambien_last = cambien_cur;
+  }
+  else if (cambien_cur == true & cambien_last == false)
+  {
+      cambien_st = XUNG_XUONG;
+      cambien_last = cambien_cur;
+  }
+  else
+  {
+      cambien_st = KHONG_DOI;
   }
 
-  if (cambien_st == true)
+  // phát hiện xung cạnh lên của cảm biến. giả sử cảm biến phát hiện vật trả về true, không trả về false.
+  if (cambien_st == XUNG_LEN)
   {
-    /* Phat hiện chai */
-
-    analogWrite(bangtai_o,0); // Dừng băng tải.
-    analogWrite(bom_o,200); // bật đông cơ bơm. thay đổi số 200 để điều chỉnh tốc độ động cơ bơm
-    delay(2000); // chờ 2000ms (2s).
-    analogWrite(bom_o,0); // tắt động cơ bơm.
-    delay(100); // chờ 100ms để đảm bảo nước trong vòi chảy hết mới chạy băng tải.
-    analogWrite(bangtai_o,200); // cho băng tải chạy lại
+      analogWrite(bangtai_o,0); // Dừng băng tải.
+      analogWrite(bom_o,200); // bật đông cơ bơm. thay đổi số 200 để điều chỉnh tốc độ động cơ bơm
+      delay(2000); // chờ 2000ms (2s).
+      analogWrite(bom_o,0); // tắt động cơ bơm.
+      delay(100); // chờ 100ms để đảm bảo nước trong vòi chảy hết mới chạy băng tải.
+      analogWrite(bangtai_o,200); // cho băng tải chạy lại
   }
 }
